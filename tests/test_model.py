e90436467279ec08a8278ae865e578c9b1bd39c8
nitishksingh24 <nitishksingh24@gmail.com>
2024-08-05 20:42:26 +0530

    add model signature test

--- tests/test_model.py
+++ tests/test_model.py
@@ -3,6 +3,8 @@
 import unittest
 import mlflow
 import os
+import pandas as pd
+import pickle
 
 class TestModelLoading(unittest.TestCase):
 
@@ -18,16 +20,19 @@ class TestModelLoading(unittest.TestCase):
 
         dagshub_url = "https://dagshub.com"
         repo_owner = "campusx-official"
-        repo_name = "mlops-mini-project"
+        repo_name = "mlops-project-2"
 
         # Set up MLflow tracking URI
         mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 
         # Load the model from MLflow model registry
-        cls.model_name = "my_modelwrgwg"
+        cls.model_name = "my_model"
         cls.model_version = cls.get_latest_model_version(cls.model_name)
         cls.model_uri = f'models:/{cls.model_name}/{cls.model_version}'
         cls.model = mlflow.pyfunc.load_model(cls.model_uri)
+
+        # Load the vectorizer
+        cls.vectorizer = pickle.load(open('models/vectorizer.pkl', 'rb'))
 
     @staticmethod
     def get_latest_model_version(model_name):
@@ -37,6 +42,22 @@ class TestModelLoading(unittest.TestCase):
 
     def test_model_loaded_properly(self):
         self.assertIsNotNone(self.model)
+
+    def test_model_signature(self):
+        # Create a dummy input for the model based on expected input shape
+        input_text = "hi how are you"
+        input_data = self.vectorizer.transform([input_text])
+        input_df = pd.DataFrame(input_data.toarray(), columns=[str(i) for i in range(input_data.shape[1])])
+
+        # Predict using the model to verify the input and output shapes
+        prediction = self.model.predict(input_df)
+
+        # Verify the input shape
+        self.assertEqual(input_df.shape[1], len(self.vectorizer.get_feature_names_out()))
+
+        # Verify the output shape (assuming binary classification with a single output)
+        self.assertEqual(len(prediction), input_df.shape[0])
+        self.assertEqual(len(prediction.shape), 1)  # Assuming a single output column for binary classification
 
 if __name__ == "__main__":
     unittest.main()
