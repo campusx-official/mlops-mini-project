9946b00b0bf5e203ba8abe7325183c39b3c7e50f
nitishksingh24 <nitishksingh24@gmail.com>
2024-08-05 20:32:30 +0530

    add model test 1

---
+++ tests/test_model.py
@@ -0,0 +1,42 @@
+# model test v1 - loading the model
+
+import unittest
+import mlflow
+import os
+
+class TestModelLoading(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        # Set up DagsHub credentials for MLflow tracking
+        dagshub_token = os.getenv("DAGSHUB_PAT")
+        if not dagshub_token:
+            raise EnvironmentError("DAGSHUB_PAT environment variable is not set")
+
+        os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+        os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+
+        dagshub_url = "https://dagshub.com"
+        repo_owner = "campusx-official"
+        repo_name = "mlops-mini-project"
+
+        # Set up MLflow tracking URI
+        mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
+
+        # Load the model from MLflow model registry
+        cls.model_name = "my_model"
+        cls.model_version = cls.get_latest_model_version(cls.model_name)
+        cls.model_uri = f'models:/{cls.model_name}/{cls.model_version}'
+        cls.model = mlflow.pyfunc.load_model(cls.model_uri)
+
+    @staticmethod
+    def get_latest_model_version(model_name):
+        client = mlflow.MlflowClient()
+        latest_version = client.get_latest_versions(model_name, stages=["Staging"])
+        return latest_version[0].version if latest_version else None
+
+    def test_model_loaded_properly(self):
+        self.assertIsNotNone(self.model)
+
+if __name__ == "__main__":
+    unittest.main()
