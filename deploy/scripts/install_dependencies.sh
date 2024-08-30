d698a1f36a20ce6ded6d829f96407216887b2cdb
nitishksingh24 <nitishksingh24@gmail.com>
2024-08-31 01:11:45 +0530

    change in install_dependencies.sh

--- deploy/scripts/install_dependencies.sh
+++ deploy/scripts/install_dependencies.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
-sudo apt-get update
+
+# Ensure the script runs in non-interactive mode
+export DEBIAN_FRONTEND=noninteractive
+
+# Update the package lists
+sudo apt-get update -y
+
+# Install Docker
 sudo apt-get install -y docker.io
+
+# Start and enable Docker service
 sudo systemctl start docker
 sudo systemctl enable docker
 
+# Install necessary utilities
 sudo apt-get install -y unzip curl
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
+
+# Download and install AWS CLI
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/home/ubuntu/awscliv2.zip"
+unzip -o /home/ubuntu/awscliv2.zip -d /home/ubuntu/
+sudo /home/ubuntu/aws/install
+
+# Add 'ubuntu' user to the 'docker' group to run Docker commands without 'sudo'
 sudo usermod -aG docker ubuntu
+
+# Login to AWS ECR
 aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 992382647488.dkr.ecr.ap-southeast-2.amazonaws.com
+
+# Clean up the AWS CLI installation files
+rm -rf /home/ubuntu/awscliv2.zip /home/ubuntu/aws
