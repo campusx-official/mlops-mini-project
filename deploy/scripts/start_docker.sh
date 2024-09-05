12631e2196f62f3d51bcb5f05509905295b523d6
nitishksingh24 <nitishksingh24@gmail.com>
2024-09-05 19:11:09 +0530

    add red background

--- deploy/scripts/start_docker.sh
+++ deploy/scripts/start_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Login to my AWS ECR
 aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 051826734860.dkr.ecr.ap-southeast-2.amazonaws.com
-docker push 051826734860.dkr.ecr.ap-southeast-2.amazonaws.com/campusx_ecr:v1
+docker pull 051826734860.dkr.ecr.ap-southeast-2.amazonaws.com/campusx_ecr:v2
 docker stop my-container || true
 docker rm my-container || true
-docker run -d -p 80:5000 -e DAGSHUB_PAT=e691c7193ab61dc9678e31c6b92ded8a65f80697 --name campusx-app 051826734860.dkr.ecr.ap-southeast-2.amazonaws.com/campusx_ecr:v1
+docker run -d -p 80:5000 -e DAGSHUB_PAT=e691c7193ab61dc9678e31c6b92ded8a65f80697 --name campusx-app 051826734860.dkr.ecr.ap-southeast-2.amazonaws.com/campusx_ecr:v2
